b31d76b8953390661f473cfaaebfce2644f25c98
Ajay Jarhad <ajayjarhad1@gmail.com>
2022-03-26 02:41:29 +0530

    feat:Added TypeORM Postgres Database;

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,48 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import * as joi from 'joi';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
+import { RestaurantsModule } from './restaurants/restaurants.module';
+import { Restaurant } from './restaurants/entities/restaurant.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'dev' && '.env.dev',
+      ignoreEnvFile: process.env.NODE_ENV === 'prod',
+      validationSchema: joi.object({
+        NODE_ENV: joi.string().valid('dev', 'prod', 'test').required(),
+        DB_HOST: joi.string().required(),
+        DB_USERNAME: joi.string().required(),
+        DB_PASSWORD: joi.string().required(),
+        DB_NAME: joi.string().required(),
+        DB_PORT: joi.number().required(),
+      }),
+    }),
+
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    RestaurantsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: process.env.NODE_ENV !== 'prod',
+      logging: process.env.NODE_ENV !== 'prod',
+      entities: [Restaurant],
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
+console.log({ process: process.env.NODE_ENV });
